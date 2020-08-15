4620ec0e4a7f370f569338a8fcb49425f2cbe4c4
PlatonB <platon.work@gmail.com>
2020-08-15 03:31:14 +0300

    Пайплайн до germline-вариантов, V3.0
    - Упрощение этапа аннотирования до замены точек на rsIDs. Настоящее аннотирование вариантов - отдельный сложный, творческий процесс, явно в эту программу не вписывающийся. Конкретно для работы пайплайна теперь не требуется устанавливать Ensembl VEP с его громоздким кэшем.
    - Этап коллинга больше не будет запрашивать ввод root-пароля. Но от вас понадобится настройка rootless-доступа к Docker. Инструкцию напишу позже.
    - Фильтрация VCF по QUAL.

--- variants_pipeline.py
+++ variants_pipeline.py
@@ -1,19 +1,19 @@
-__version__ = 'V2.1'
+__version__ = 'V3.0'
 
-print('''
+print(f'''
 Python3-программа, формирующая пайплайн
-от ридов до характеристик SNPs.
+от ридов до коротких генеративных вариантов.
 
-Автор: Платон Быкадоров (platon.work@gmail.com), 2019-2020.
-Версия: V2.1.
-Лицензия: GNU General Public License version 3.
+Автор: Платон Быкадоров (platon.work@gmail.com), 2019-2020
+Версия: {__version__}
+Лицензия: GNU General Public License version 3
 Поддержать проект: https://money.yandex.ru/to/41001832285976
 Документация: https://github.com/PlatonB/ngs-pipelines/blob/master/README.md
 Багрепорты/пожелания/общение: https://github.com/PlatonB/ngs-pipelines/issues
 Справка по CLI: python3 variants_pipeline.py -h
 
 Упрощённая схема пайплайна:
-Выравнивание --> коллинг --> (только для человека) аннотирование.
+Выравнивание --> коллинг --> (только для человека) поиск rsIDs.
 
 На вход идут FASTA/Q-файлы с исследуемыми
 ридами (WGS, WES, PacBio) и один
@@ -37,25 +37,28 @@ SA1016X3_3L_R1.fastq.gz
 SA1016X3_3L_R2.fastq.gz
 ''')
 
-import sys, os
+import sys, os, re, gzip
 sys.dont_write_bytecode = True
 from backend.core import add_main_args, Core, run_command
+from backend.prep_dbsnp_data import prep_dbsnp_data
+from contextlib import ExitStack
+from pysam import TabixFile, asTuple
 
 #Добавление кросспайплайновых аргументов.
 argparser = add_main_args()
 
 #Специфическим аргументом
 #этого пайплайна будет
-#запрашиваемый программой
+#запрашиваемый коллером
 #DeepVariant тип секвенирования.
-argparser.add_argument('-s', '--seq-type', metavar='[wgs]', default='wgs', choices=['wgs', 'wes', 'pacbio'], dest='seq_type', type=str,
+argparser.add_argument('-s', '--seq-type', metavar='[wgs]', choices=['wgs', 'wes', 'pacbio'], default='wgs', dest='seq_type', type=str,
                        help='{wgs, wes, pacbio} Тип секвенирования')
 
-#К парсингу разумно
-#приступать только один
-#раз, и притом после
-#окончания формирования
-#набора аргументов.
+#К парсингу аргументов
+#разумно приступать
+#только один раз, и
+#притом после окончания
+#формирования их набора.
 #Если парсить несколько
 #раз, исследователю
 #станет доступна только
@@ -76,10 +79,16 @@ core.seq_type = args.seq_type.upper()
 
 #Архивация (BGZIP) и индексация
 #(Bowtie2 и Samtools) FASTA/Q
-#референсного генома, группирование
+#референсного генома, скачивание
+#данных dbSNP и формирование их
+#урезанной версии, группировка
 #имён исследуемых FASTA/Q.
+print(f'\n{core.ref_file_name}: сжатие и индексация\n')
 core.compress_ref_file()
 core.index_ref_file()
+if core.species_name == 'homo_sapiens':
+        print('\ndbSNP: скачивание и обработка')
+        dbsnp_tsv_paths = prep_dbsnp_data(core.ref_dir_path, core.threads_quan)
 nested_file_names = core.group_file_names()
 
 #Перебор списков, в каждом из
@@ -88,6 +97,7 @@ for element in nested_file_names:
         
         #Выравнивание, конвертация SAM в
         #BAM, сортировка и индексация BAM.
+        print(f'\n{", ".join(element)}: выравнивание, SAM --> BAM, сортировка и индексация BAM')
         bam_file_base_path = core.get_srtd_bam(element)
         
         #Неинтересная возня с путями к файлам.
@@ -96,27 +106,100 @@ for element in nested_file_names:
         if bam_file_base_path == None:
                 continue
         trg_dir_path, bam_file_name = os.path.split(f'{bam_file_base_path}_srtd.bam')
-        vcf_file_path = f'{bam_file_base_path}.vcf.gz'
-        vcf_file_name = os.path.basename(vcf_file_path)
-        ann_file_path = f'{bam_file_base_path}_ann.tsv'
+        rawvcf_file_path = f'{bam_file_base_path}.vcf.gz'
+        rawvcf_file_name = os.path.basename(rawvcf_file_path)
+        vcf_file_path = f'{bam_file_base_path}_ann.vcf'
         
         #По BAM и референсному геному
         #осуществляется коллинг SNPs.
-        #SNPs человеческого генома
-        #ещё и подробно аннотируются.
+        print(f'\n{bam_file_name}: коллинг')
         run_command(f'''
-sudo docker run \
+docker run \
 -v "{core.ref_dir_path}":"/ref" \
 -v "{trg_dir_path}":"/trg" \
 google/deepvariant /opt/deepvariant/bin/run_deepvariant \
 --num_shards={core.threads_quan} --model_type={core.seq_type} \
 --ref=/ref/{core.ref_file_name} \
 --reads=/trg/{bam_file_name} \
---output_vcf=/trg/{vcf_file_name}''')
+--output_vcf=/trg/{rawvcf_file_name}''')
+        
+        #Ниже будет код, применимый
+        #только к геному человека.
         if core.species_name != 'homo_sapiens':
                 continue
-        run_command(f'''
-vep --cache --fork {core.threads_quan} --quiet --no_stats --tab --offline -e \
--i {vcf_file_path} -o {ann_file_path}''')
-        os.remove(vcf_file_path)
-        os.remove(vcf_file_path + '.tbi')
+        
+        #Полученный в результате
+        #коллинга VCF не содержит
+        #идентификаторов вариантов.
+        #В случае человеческого генома
+        #их можно получить по dbSNP,
+        #что будет сделано ниже.
+        print(f'\n{rawvcf_file_name}: фильтрация и замена точек на rsIDs')
+        with gzip.open(rawvcf_file_path, mode='rt') as rawvcf_file_opened:
+                
+                #Предполагается, что варианты
+                #dbSNP распределены по отдельным
+                #файлам (1 архив - 1 хромосома).
+                #По этой причине для разных
+                #аннотируемых SNPs нужно доставать
+                #rsIDs из разных dbSNP-архивов.
+                #Многочисленные открытия-закрытия
+                #архивов займут много времени,
+                #поэтому откроем на чтение
+                #парсером pysam их всех сразу.
+                with ExitStack() as stack:
+                        dbsnp_tsvs_opened = {os.path.basename(dbsnp_tsv_path)[:-7]:
+                                             stack.enter_context(TabixFile(dbsnp_tsv_path)) for dbsnp_tsv_path in dbsnp_tsv_paths}
+                        
+                        #Открытие конечного VCF
+                        #на чтение и перегонка в него
+                        #хэдеров свежеколленного VCF.
+                        with open(vcf_file_path, 'w') as vcf_file_opened:
+                                for line in rawvcf_file_opened:
+                                        if line.startswith('##'):
+                                                vcf_file_opened.write(line)
+                                        else:
+                                                vcf_file_opened.write(line)
+                                                break
+                                        
+                                #В dbSNP-VCF может найтись
+                                #несколько соответствий
+                                #координате текущего
+                                #запрашиваемого варианта.
+                                #Как правило, это когда
+                                #для данной позиции известны
+                                #несколько разных вставок.
+                                #Ради выявления вхождения
+                                #наколленного варианта
+                                #в dbSNP-референс сверяются
+                                #не только координаты, но
+                                #и соответствующие аллели.
+                                #Если вхождение обнаружилось
+                                #по координате и подтвердилось
+                                #по аллелям, точка в сыром VCF
+                                #заменяется на rsID из dbSNP.
+                                for line in rawvcf_file_opened:
+                                        row = line.split('\t')
+                                        chrom, pos, qual = row[0], int(row[1]), float(row[5])
+                                        if qual < 20:
+                                                continue
+                                        try:
+                                                for tup in dbsnp_tsvs_opened[chrom].fetch(chrom,
+                                                                                          pos - 1,
+                                                                                          pos,
+                                                                                          parser=asTuple()):
+                                                        if row[3] == tup[3] and row[4] == tup[4]:
+                                                                row[2] = tup[2]
+                                                                vcf_file_opened.write('\t'.join(row))
+                                                                break
+                                                else:
+                                                        vcf_file_opened.write(line)
+                                        except KeyError:
+                                                vcf_file_opened.write(line)
+                                                
+        #BGZIP-архивация окончательного VCF и
+        #удаление вместе с индексом сырого VCF.
+        print(f'\n{os.path.basename(vcf_file_path)}: сжатие')
+        run_command(f'bgzip -@ {core.threads_quan} -l 9 -i {vcf_file_path}')
+        os.remove(rawvcf_file_path)
+        os.remove(rawvcf_file_path + '.tbi')
